115569dc17518b7affa4b92954e4329e19ce8f1f
Hendey Fan <hendeyfan98@gmail.com>
2022-11-12 22:47:03 +0800

    update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const fs = require('fs');
 const Redis = require('redis')
 const redisClient = Redis.createClient()
+redisClient.connect()
 
 port = 3000
 const DEFAULT_EXPIRATION = 600
@@ -11,11 +12,17 @@ const app = express()
 app.use(cors())
 
 app.get("/", async(req, res) => {
-    // res.sendFile('mock_small.json', {root: __dirname})
-    fs.readFile('MOCK_DATA.json', 'utf8', (err, data) => {
-        redisClient.setEx("data", DEFAULT_EXPIRATION, data)
-        res.json(JSON.parse(data))
-    })
+    console.log(`req recv`)
+    data = await redisClient.get('data')
+    if (data != null) {
+        console.log(`cache hit`)
+        return res.json(JSON.parse(data))
+    } else {
+        console.log(`cache miss`)
+        data = fs.readFileSync('MOCK_DATA.json', 'utf8')
+        redisClient.setEx('data', DEFAULT_EXPIRATION, data)
+        return res.json(JSON.parse(data))
+    }
 })
 
 app.listen(port, () => {
